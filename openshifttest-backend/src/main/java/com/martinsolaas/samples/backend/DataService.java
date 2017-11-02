8ef7e8e7c18c80de2a0cf43284b95b2c086acfcc
vsasjms <JMS@VPS.NO>
2017-11-02 15:12:44 +0100

    initial

---
+++ openshifttest-backend/src/main/java/com/martinsolaas/samples/backend/DataService.java
@@ -0,0 +1,29 @@
+package com.martinsolaas.samples.backend;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import com.martinsolaas.samples.backend.data.Category;
+import com.martinsolaas.samples.backend.data.Product;
+import com.martinsolaas.samples.backend.mock.MockDataService;
+
+/**
+ * Back-end service interface for retrieving and updating product data.
+ */
+public abstract class DataService implements Serializable {
+
+    public abstract Collection<Product> getAllProducts();
+
+    public abstract Collection<Category> getAllCategories();
+
+    public abstract void updateProduct(Product p);
+
+    public abstract void deleteProduct(int productId);
+
+    public abstract Product getProductById(int productId);
+
+    public static DataService get() {
+        return MockDataService.getInstance();
+    }
+
+}
